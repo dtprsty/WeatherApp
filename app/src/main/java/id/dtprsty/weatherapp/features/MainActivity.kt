67a4802657ecbb06db84a485205c71cb14152ab9
Dwi Teguh Prasetyo <dtprsty@gmail.com>
2020-02-28 20:17:18 +0700

    init commit

--- app/src/main/java/id/dtprsty/weatherapp/features/MainActivity.kt
+++ app/src/main/java/id/dtprsty/weatherapp/features/MainActivity.kt
@@ -2,13 +2,53 @@ package id.dtprsty.weatherapp.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import id.dtprsty.weatherapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
 
+    private fun initView(){
+        val sheetBehavior = BottomSheetBehavior.from<ConstraintLayout>(bottom_sheet)
+        sheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        ivArrow.animate().rotation(180f).start()
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        ivArrow.animate().rotation(0f).start()
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
+
+        ivArrow.setOnClickListener {
+            if (sheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                ivArrow.animate().rotation(180f).start()
+            } else {
+                sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                ivArrow.animate().rotation(0f).start()
+            }
+        }
     }
 }
